c8f42d847945e2cf5a068a706a8dfabb2b3f8ae4
Jules de Smit <julesdesmit@gmail.com>
2022-01-29 19:23:07 -0800

    Remove header file

--- src/main.c
+++ src/main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-#include "network.h"
+#include "network.c"
 
 int main() {
-	int *hidden_layer = create_layer(1, 2);
-	int *output_layer = create_layer(2, 1);
+	double* hidden_layer = create_layer(1, 2);
+	double* output_layer = create_layer(2, 1);
 	for (int i = 0; i < 3; i++) {
-		printf("%d ", *(hidden_layer + i));
+		printf("%f ", *(hidden_layer + i));
 	}
 
 	printf("\n");
 	for (int i = 0; i < 2; i++) {
-		printf("%d ", *(output_layer + i));
+		printf("%f ", *(output_layer + i));
 	}
 
 	printf("\n");
 	for (int i = 0; i < 2; i++) {
-		printf("%d ", *(output_layer + 2 + i));
+		printf("%f ", *(output_layer + 2 + i));
 	}
 
 	printf("\n");
--- src/network.h
+++
@@ -1,5 +0,0 @@
-int* create_layer(int n_weights, int n_neurons);
-int* forward_propagate(int* hidden_layer, int hidden_neurons, int hidden_weights, int* output_layer, int output_neurons, int output_weights, int* inputs, int inputs_length);
-double activate(int *weights, int *inputs, int inputs_len);
-double transfer(double activation);
-double transfer_derivative(double output);
